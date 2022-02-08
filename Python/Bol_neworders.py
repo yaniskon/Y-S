7ec41bb00ead20911494a2433a6180c622052d59
Yanis <konstantinidisj@gmail.com>
2022-02-08 22:27:21 +0100

    neworders and offers imporvements

--- Python/Bol_neworders.py
+++ Python/Bol_neworders.py
@@ -4,8 +4,7 @@ import json
 import time
 import pandas as pd
 import os
-# from pymongo import MongoClient
-# import pymongo
+import pymongo
 
 
 def get_new_orders():
@@ -153,18 +152,62 @@ def ship_order_item(orderId, shippingLabel):
   'Content-Type': 'application/vnd.retailer.v6+json',
   'Authorization': 'Bearer ' + get_token() 
   }
+  shipRef = input("Enter your FA-: ")
   payload = json.dumps({
   "orderItems": [
     {
       "orderItemId": orderId
     }
   ],
-  "shipmentReference": "FA-0000605",
+  "shipmentReference": shipRef,
   "shippingLabelId": shippingLabel
 })
   response = requests.request("PUT", url, headers=headers, data=payload)
 
   print(response.text)
+
+
+
+def get_orderid():
+  url = "https://api.bol.com/retailer/orders"
+  payload={}
+  headers = {
+  'Accept': 'application/vnd.retailer.v6+json',
+  'Authorization': 'Bearer ' + get_token() 
+  }
+  response = requests.request("GET", url , headers=headers, data=payload)
+
+  response_dict = json.loads(response.text)
+  print(response_dict)
+  orderIds = []
+  for order in response_dict['orders']:
+    orderIds.append(order['orderId'])
+  return orderIds
+
+
+def get_orderid_data(orderId):
+  myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+  mydb = myclient["bol"]
+  myorderinfo = mydb["orderInfo"]
+
+  url = 'https://api.bol.com/retailer/orders/' + orderId
+  payload={}
+  headers = {
+  'Accept': 'application/vnd.retailer.v6+json',
+  'Authorization': 'Bearer ' + get_token() 
+}
+
+  response = requests.request("GET", url, headers=headers, data=payload)
+  response_dict = json.loads(response.text)
+  if myorderinfo.find_one({"orderId": orderId}) == None:
+    myorderinfo.insert_one(response_dict)
+
+
+try:
+  for order in get_orderid():
+    get_orderid_data(order)
+except KeyError:
+  print("No new orders!")
 
 # print(get_new_orders())
 for orderItemId, ean, quantity, quantityShipped, quantityCancelled in get_new_orders():
@@ -172,72 +215,4 @@ for orderItemId, ean, quantity, quantityShipped, quantityCancelled in get_new_or
   for shippingLabelOfferId in result[orderItemId]:
     shipingLabel = get_process(Create_a_shipping_label(orderItemId, shippingLabelOfferId))
     get_shipping_label(shipingLabel)
-  #   ship_order_item(item, shipingLabel)
-  #   break
-
-
-
-
-
-
-
-# if orders_list != []:
-#   for order in orders_list:
-#     print(order)
-#     print("+=================================================================+")
-#     orderItemId = order["orderItems"][0]["orderItemId"] # this support only single order 
-#     print(orderItemId)
-#     delivery_options(orderItemId)
-
-
-
-# # # # Mongo db
-
-# # # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-# # # mydb = myclient["bol"]
-# # # myorder = mydb["orders"]
-
-
-# # # new_orders = True
-
-# # # while new_orders:  # We can paginate this API up until 3 months worth of shipments
-
-# # #     # don't break bol
-# # #     rate_limit = response.headers["X-RateLimit-Remaining"]
-# # #     reset = response.headers["X-RateLimit-Reset"]
-# # #     if int(rate_limit) < 1:
-# # #         print("Good night!")
-# # #         time.sleep(int(reset))
-# # #         print("Back there again!")
-    
-# # #     #print(response.json())
-# # #     response_dict = json.loads(response.text)
-# # #     print("+=============================++++")
-# # #     print(response_dict)
-# # #     print("+=============================++++")
-    
-# # #     if response_dict != {}:
-# # #       for x in response_dict['orders']:
-# # #         print("+=======", x['orderId'], "=======", x['orderItems'][0]) # this [0] is for order that contain a single item
-# # #         orderitemID = x['orderItems'][0]['orderItemId'] # this should contain the second order
-# # #         payload2 = json.dumps({
-# # #   "orderItems": [
-# # #     {
-# # #       "orderItemId": orderitemID
-# # #     }
-# # #   ]
-# # # })
-# # #         print(payload)
-# # #         headers2 = {
-# # #   'Accept': 'application/vnd.retailer.v6+json',
-# # #   'Content-Type': 'application/vnd.retailer.v6+json',
-# # #   'Authorization': 'Bearer ' + get_token() 
-# # # }
-# # #         print(headers)
-# # #         enrichedurl2 = 'https://api.bol.com/retailer/shipping-labels/delivery-options'
-# # #         response1 = requests.request("POST", enrichedurl2 , headers=headers2, data=payload2)
-# # #         print(response1.text)
-# # #         #"shippingLabelOfferId" : "4ea7ebe2-1c31-4bf8-8cbf-21c6ec9e964e",
-# # #     else:
-# # #       bool_results = False
-# # #     page += 1
+    ship_order_item(orderItemId, shipingLabel)
--- Python/Bol_offers.py
+++ Python/Bol_offers.py
@@ -9,30 +9,95 @@ import pandas as pd
 
 # Standard API parameters
 
-url = "https://api.bol.com/retailer/offers/export"
-payload=json.dumps({
+def request_offers_export():
+  url = "https://api.bol.com/retailer/offers/export"
+  payload=json.dumps({
 
 "format": "CSV"
 
 })
-headers = {
+  headers = {
    'Content-Type': 'application/vnd.retailer.v6+json',
   'Accept': 'application/vnd.retailer.v6+json',
-  'Authorization': ''
+  'Authorization': 'Bearer ' + get_token()
 }
-headers['Authorization'] = 'Bearer ' + get_token() 
+
+  response = requests.request("POST", url , headers=headers, data=payload)
+  print(response.text)
+  response_dict = json.loads(response.text)
+  processID = response_dict['processStatusId']
+  time.sleep(5)
+  return processID
+
+def get_offers_export(processID):
+
+  payload= {}
+  headers = {
+    'Accept': 'application/vnd.retailer.v6+json',
+    'Authorization': 'Bearer ' + get_token() 
+  }
+  process_url = "https://api.bol.com/retailer/process-status/" + processID 
+  response = requests.request("GET", process_url , headers=headers, data=payload)
+  response_dict = json.loads(response.text)
+  print(response_dict)
+  entityId = response_dict['entityId']
+  print("+================line46=================+")
+  return entityId
 
 
+def retrieve_an_offer_export_file(entityId):
+  url = "https://api.bol.com/retailer/offers/export/" +entityId
+  payload= {}
+  headers = {
+    'Accept': 'application/vnd.retailer.v6+csv',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Bearer ' + get_token() 
+  }
+  response = requests.request("GET", url , headers=headers, data=payload)
+  data = response.text
+  df = pd.DataFrame([x.split(',') for x in data.split('\n')])
+  print(df)
+  df.to_csv('offers.csv')
+  df = pd.read_csv('offers.csv')
+  return df
+
+
+
+def getting_offerId_data(df):
+
+  myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+  mydb = myclient["bol"]
+  myoffers = mydb["offers"]
+
+  data = df.values.tolist()
+  i = 2
+  while i < len(data) -1:
+    offerId = data[i][1]
+    if myoffers.find_one({"offerId": offerId}) == None:
+      url = 'https://api.bol.com/retailer/offers/' + offerId
+      payload= {}
+      headers = {
+      'Accept': 'application/vnd.retailer.v6+json',
+      'Authorization': 'Bearer ' + get_token() 
+    }
+      response = requests.request("GET", url, headers=headers, data=payload)
+
+      response_dict = json.loads(response.text)
+      myoffers.insert_one(response_dict)
+    i += 1
+
+
+df = pd.read_csv(r'C:\Users\konst\Documents\Y-S\Python\offers.csv')
+print(df)
+getting_offerId_data(df)
+# getting_offerId_data(retrieve_an_offer_export_file(get_offers_export(request_offers_export())))
 # Step 1
 # First you need a request offers export which will start the creation of the offer export file. The output format of the export is a CSV file.
 
-response = requests.request("POST", url , headers=headers, data=payload)
-print(response.text)
-response_dict = json.loads(response.text)
 # print(response_dict)
-processID = response_dict['processStatusId']
-print(processID)
-time.sleep(10)
+
+# print(processID)
+
 
 # Step 2
 # In the response to the previous request you can find the processID.
@@ -42,47 +107,13 @@ time.sleep(10)
 #If you successfully retrieve the process status, you should get a response with an entityId. The entityId will contain the ID needed to retrieve the export file.
 
 
-payload= {}
-headers = {
-  'Accept': 'application/vnd.retailer.v6+json',
-  'Authorization': ''
-}
-headers['Authorization'] = 'Bearer ' + get_token() 
-process_url = "https://api.bol.com/retailer/process-status/" + processID 
-response = requests.request("GET", process_url , headers=headers, data=payload)
-response_dict = json.loads(response.text)
-print(response_dict)
-entityId = response_dict['entityId']
-print("+================line46=================+")
-print(entityId)
-
 
 # Step 4
 # Once the status field for the retrieve process status contains the value SUCCESS, you can use the entityId (which is the ID for the offer export) in combination with the retrieve offer export call. The response will be the requested CSV file which will contain both EANs and offerId’s which can be used to map to the offer IDs in your database.
 
 
-url = "https://api.bol.com/retailer/offers/export/" + entityId
 
-payload={}
-headers = {
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'Accept': 'application/vnd.retailer.v6+csv',
-  'Authorization': 'Bearer '+ get_token()
-}
 
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
-print("+=====telos========+")
-
-data = response.text
-df = pd.DataFrame([x.split(',') for x in data.split('\n')])
-print(df)
-df.to_csv('offers.csv')
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["bol"]
-myproducts = mydb["products"]
 
 # response = requests.request("POST", url , headers=headers, data=payload)
 # print(response)
